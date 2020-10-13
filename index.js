4514a3760c50f6e4def3603059db5ed803b5ab18
Csaba Csikász <uborkasteszta@gmail.com>
2020-10-13 12:50:44 +0200

    Firt commit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// mobile menu
+// Mobile menu
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
+const konyvekDropdown = document.querySelector('#konyvekDropdown');
+const adminDropdown = document.querySelector('#adminDropdown');
 
-burgerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
+burgerIcon.addEventListener('click' , () => {
+    navbarMenu.classList.toggle('is-active');
+});
+
+
+// Menu dropdowns
+konyvekDropdown.addEventListener('click' , () => {  
+    konyvekDropdown.classList.toggle('is-active');
+});
+
+adminDropdown.addEventListener('click' , () => {  
+    adminDropdown.classList.toggle('is-active');
 });
